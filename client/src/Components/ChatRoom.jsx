7cd30c36a9d47dcbdff844eeb82c35cdc44e34d3
Kavin-U <ukavin7@gmail.com>
2024-11-22 18:52:12 +0530

    More UI Progress

--- client/src/Components/ChatRoom.jsx
+++ client/src/Components/ChatRoom.jsx
@@ -2,22 +2,38 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 function ChatRoom() {
-  const Rooms = [1,2];
+  const Rooms = [1, 2];
+
+  
+
+
+
   return (
-    <div className="w-[100%] h-screen bg-teal-800 flex items-center justify-end pr-2">
-      <div className="w-[95%] h-[89%] bg-teal-900 flex">
-        <div className="bg-teal-600 w-fit h-full">
-          {Rooms.map((val) => {
-            return (
-              <Link key={val} to={`rooms/${val}`} className="text-white block">
-                <div className="py-5 px-10 hover:bg-green-800 cursor-pointer ">
-                  Rooms {[val]}
-                </div>
-              </Link>
-            );
-          })}
+    <div className="w-[100%] h-[91.4vh] 2xl:h-[94.4vh] bg-teal-800 flex items-center">
+      <div className="w-[95%] h-[89%] bg-teal-900 flex mx-auto">
+        <div className="bg-teal-600 w-fit h-full flex justify-between flex-col">
+          <div>
+            {Rooms.map((val) => {
+              return (
+                <Link
+                  key={val}
+                  to={`rooms/${val}`}
+                  className="text-white block"
+                >
+                  <div className="py-4 hover:bg-green-800 text-center cursor-pointer ">
+                    Room {[val]}
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <div className="mx-auto py-2">
+            <button className="text-gray-700 w-[200px] m-2 font-semibold rounded-lg text-center bg-teal-400 py-5">
+              New Chat Room
+            </button>
+          </div>
         </div>
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
   );
--- client/src/Pages/Rooms.jsx
+++ client/src/Pages/Rooms.jsx
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom'
 export default function Rooms() {
    const params = useParams()
   return (
-    <div>
-        <h1>Rooms {params.RoomId}</h1>
+    <div className='w-full h-[50px] flex items-center px-5 bg-teal-950'>
+        <h1 className='text-white'>Room {params.RoomId}</h1>
     </div>
   )
 }
